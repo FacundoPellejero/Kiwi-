4e00201e7fb4d09887a624f64c98bd0adb8d3294
75916-Facundo-Pellejero <facundopellejero.nt@gmail.com>
2022-09-21 16:58:30 -0300

    First commit
    KIWI 🥝

---
+++ index.js
@@ -0,0 +1,59 @@
+
+const { Client, GatewayIntentBits, Partials, ActivityType } = require('discord.js');
+const { token, kiwi } = require('./config.json');
+const activities = [`NOW WITH EXTRA KIWIS!! 🥝`, `🥝🥝🥝🥝🥝`, `KIWI 🥝 KIWI 🥝 KIWI 🥝`, `MINECRAFT (WITH KIWI) 🥝😼`, `no se algo con kiwis 🥝🥝🥝`, `Aaahh!!! Real Kiwis 🥝👺`, `🥝🥝SANKLO PELADO PUTO🥝🥝`];
+
+
+const client = new Client({
+    intents: [
+      GatewayIntentBits.DirectMessages,
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildBans,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.MessageContent,
+      GatewayIntentBits.GuildPresences,
+      GatewayIntentBits.GuildMessageTyping
+    ],
+    partials: [Partials.Channel],
+  });
+
+
+client.on('ready', () => {
+    
+    client.user.setPresence({
+        activities: [{ name: `ONAAA`, type: ActivityType.Playing }],
+        status: 'idle',
+      });
+    
+    setInterval(() => {
+	    const randomIndexAc = Math.floor(Math.random() * (activities.length - 1) + 1);
+		const newActivity = activities[randomIndexAc];
+
+		client.user.setPresence({
+            activities: [{ name: newActivity, type: ActivityType.Playing }],
+            status: 'idle',
+          });
+	}, 2400000);
+	console.log('KIWI!');
+    //2400000
+});
+
+
+client.on('messageCreate', async (message) =>{
+    //debug 
+    //console.log(message);
+    if(message.author.bot) return;
+    if (message.author.id === kiwi){
+        try {
+            message.react('🥝');
+        } catch (error) {
+            console.log('kiwi borro el mensaje' + error)
+        }
+    }
+    else{
+        console.log('no es kiwi');
+    }
+})
+
+
+client.login(token);
